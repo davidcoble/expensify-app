9fcbb6814af981e54af558ab523b3ec3d8a8f59a
Dave Coble <d.coble@f5.com>
2018-04-09 17:48:46 -0700

    fetching data from firebase

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+import {startSetExpenses} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -13,10 +13,15 @@ import './firebase/firebase';
 
 const store = configureStore();
 
+
 const jsx = (
-  <Provider store={store}>
-    <AppRouter />
-  </Provider>
+    <Provider store={store}>
+        <AppRouter/>
+    </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
